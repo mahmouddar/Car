a86edb1c35a1e3b9bb5740ee4c729df90a5b8397
mahmoud darwish <mahmouddarwich3@gmail.com>
2019-01-08 17:45:29 +0200

    last update 8/1

--- src/main/java/com/example/CarApp/domain/Trip.java
+++ src/main/java/com/example/CarApp/domain/Trip.java
@@ -1,5 +1,64 @@
 package com.example.CarApp.domain;
 
-public class Trip {
+import java.util.HashSet;
 
+import java.util.Set;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Trip {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long tripid;
+	private String name;
+
+	@ManyToMany(mappedBy = "trips") 
+     private Set<Car> cars = new HashSet<Car>(0);
+	public Trip() {
+
+	}
+
+	public Trip(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public Trip(String name,Set<Car> cars) {
+		super();
+		this.name = name;
+		this.setCars(cars);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public long getTripid() {
+		return tripid;
+	}
+
+	public void setTripid(long tripid) {
+		this.tripid = tripid;
+	}
+
+	public Set<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(Set<Car> cars) {
+		this.cars = cars;
+	}
 }
---
+++ src/main/java/com/example/CarApp/domain/TripRepository.java
@@ -0,0 +1,7 @@
+package com.example.CarApp.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TripRepository extends CrudRepository<Trip, Long> {
+
+}
